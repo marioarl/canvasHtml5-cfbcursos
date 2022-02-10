cd5be07d86b6fb8b8b9d000697bdb547dfc0fcfa
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-10 08:21:39 -0300

    criacao script-c.js

---
+++ aula 02 - rect clearRect fillRect/script-c.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("desenho1")
+var ctx=canvas.getContext('2d')
+var px = 168
+var py = 50
+
+//Quando se aumentam as variaveis, o quadrado se movimenta
+ctx.fillStyle="#00f" 
+ctx.rect(px,py,200,200) 
+ctx.fill()
+
+
+

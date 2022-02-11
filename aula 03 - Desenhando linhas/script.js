c46c7822a7ac4e5967aae9a42c6a4aa56af0fc5f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-11 20:56:53 -0300

    Criacao script.js

---
+++ aula 03 - Desenhando linhas/script.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("desenho1")
+var ctx=canvas.getContext('2d')
+
+//Desenhar a linha
+
+
+ctx.moveTo(0,0)//Mover o apontador para o inicio
+ctx.lineTo(250,250) //x(x quando aumenta vai para a direita) , y(quando aumenta o y a linha desce e quando abaixo o y a linha sobe) 
+ctx.lineTo(400,250)
+ctx.lineTo(400,100)
+ctx.stroke()

6e99d42882fd4865a8480d471e1f983a0d5722d6
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-16 18:16:47 -0300

    Criacao script.js

---
+++ aula 04 - Movimentar elementos/script.js
@@ -0,0 +1,19 @@
+
+let px=0
+let anima
+
+function desenha() {
+    var canvas=document.getElementById("desenho1")
+    var ctx = canvas.getContext('2d')
+    ctx.clearRect(0,0,500,500)    
+
+    ctx.fillStyle='#f00'
+    ctx.fillRect(px,0,50,50)
+
+    px++
+
+    anima = requestAnimationFrame(desenha)
+}
+
+
+

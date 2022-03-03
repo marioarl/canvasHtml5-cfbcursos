0c98d5199b36095fc2b81d3bb2fa83e63cb739f0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-03 16:28:11 -0300

    Criacao script.js

---
+++ aula 10 - Inserindo imagens/script.js
@@ -0,0 +1,44 @@
+let canvas = document.getElementById("desenho1")
+let ctx=canvas.getContext('2d')
+
+let nave = new Image()
+nave.src = "nave.jpg"
+nave.onload = ()=>{
+    ctx.drawImage(nave,0,0,50,50) 
+    /*
+    Parametros
+    1) Imagem
+    2) X inicial do recorte
+    3) Y inicial do recorte
+    4) Largura do recorte
+    5) Altura do recorte
+    6) Posicao X da imagem
+    7) Posicao Y da imagem
+    8) Largura da imagem
+    9) Altura da imagem
+    */
+
+}
+
+let cores = new Image()
+cores.src="cores2.jpg"
+cores.addEventListener('load', ()=>{
+    ctx.drawImage(cores,100,0,50,50)
+})
+
+/*
+Ou pode utilizar o addEventListener
+
+nave.addEventListener('load', ()=>{
+    ctx.drawImage(nave,0,0)
+})
+*/
+
+
+//Indicando o tamanho nos parametros do drawImage
+/*
+nave.onload = ()=>{
+    ctx.drawImage(nave, 0, 0, 50 ,50) //Com parametros de tamanho da imagem
+}
+*/
+

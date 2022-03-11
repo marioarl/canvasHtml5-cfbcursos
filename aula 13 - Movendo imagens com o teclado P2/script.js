67ca9a57d7bbb7e8246bdd79de0f37f5d8eae642
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-11 15:40:32 -0300

    Create script.js

---
+++ aula 13 - Movendo imagens com o teclado P2/script.js
@@ -0,0 +1,96 @@
+let canvas = document.getElementById("desenho1")
+let ctx=canvas.getContext('2d')
+
+let jogador = new Image()
+jogador.src="ogre.png"
+let numSprite = 0
+let posIniX = 0
+let limagem = 0 
+let largSprite = 0
+let numSprites = 4
+let posX = 0
+let posY = 0
+let velocidade = 2
+let cima = false, baixo = false, esquerda = false, direita = false
+
+jogador.addEventListener('load', ()=>{
+    largSprite = jogador.width / numSprites
+    altSprite = jogador.height / numSprites
+    posIniX = largSprite * numSprite //Numero do sprite
+    ctx.drawImage(jogador,posIniX,0,largSprite,altSprite,posX,posY,largSprite,altSprite)
+})
+
+/*
+let anima = setInterval(()=>{
+    ctx.clearRect(0,0,500,500)
+    numSprite++
+    if(numSprite > 3)
+        numSprite = 0
+    posIniX = largSprite * numSprite
+    ctx.drawImage(jogador,posIniX,0,largSprite,altSprite,posX,posY,largSprite,altSprite)
+},200)
+*/
+
+let desenha=()=>{
+    ctx.clearRect(0,0,500,500)
+    numSprite++
+    ctx.drawImage(jogador,posIniX,0,largSprite,altSprite,posX,posY,largSprite,altSprite)
+}
+
+
+//Adicionar evento para tecla
+
+window.addEventListener('keydown', (event)=>{
+    if(event.keyCode == 37){ //Esquerda
+        esquerda = true
+    }else if (event.keyCode == 39){ //Direita
+        direita = true
+    }
+
+    if(event.keyCode == 38){ //Cima
+        cima = true
+    }else if (event.keyCode == 40){ //Baixo
+        baixo = true
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    if(event.keyCode == 37){ //Esquerda
+        esquerda = false
+    }else if (event.keyCode == 39){ //Direita
+        direita = false
+    }
+
+    if(event.keyCode == 38){ //Cima
+        cima = false
+    }else if (event.keyCode == 40){ //Baixo
+        baixo = false
+    }
+})
+
+//Funcao para verificar o valores destas variaveis e atualizar a posicao
+
+const game=()=>{
+    if(cima){
+        posY -= velocidade
+    }
+    if(baixo){
+    posY += velocidade
+    }
+
+    if(esquerda){
+        posX -= velocidade
+    }
+    if(direita){
+        posX += velocidade
+    }
+    requestAnimationFrame(game)
+    desenha()
+    
+}
+
+requestAnimationFrame(game)
+
+
+
+ 

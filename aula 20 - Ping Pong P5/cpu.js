1076686280e2aec9beb002488cde53c822b1fb1f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-04-07 09:43:32 -0300

    Criacao cpu.js

---
+++ aula 20 - Ping Pong P5/cpu.js
@@ -0,0 +1,59 @@
+class Cpu{
+    constructor(ctx,bola){
+        this.ctx=ctx
+        this.bola=bola
+        this.vel=2.5
+        this.largura=20
+        this.altura=100
+        this.x=(this.ctx.canvas.width-this.largura)
+        this.y=(this.ctx.canvas.height / 2)-(this.altura / 2)
+        this.inicioX=this.x
+        this.inicioY=this.y
+    }
+
+    gerenciar(){
+        if(this.bola.dirx > 0 && this.bola.x > this.ctx.canvas.width/2){
+            //Rebater a bola
+            if(this.bola.y + (this.bola.altura/2) > this.y+(this.altura/2)){
+                this.y+=this.vel
+            }
+            if(this.bola.y + (this.bola.altura/2) < this.y+(this.altura/2)){
+                this.y-=this.vel
+            }
+            if(this.bola.x+this.bola.largura < this.x){
+                this.x-=this.vel
+            } 
+
+        }else{
+            //Ir para a posicao de origem
+            if(this.y > this.inicioY){
+                this.y-=this.vel
+            }
+            if(this.y < this.inicioY){
+                this.y+=this.vel
+            }
+            if(this.x > this.inicioX){
+                this.x-=this.vel
+            }
+            if(this.x < this.inicioX){
+                this.x+=this.vel
+            }
+        }
+        //Colisao com o cpu
+        if(
+            (this.x <= this.bola.x + this.bola.largura && this.x+this.largura >= this.bola.x) &&
+            (this.y+this.altura >= this.bola.y && this.y <= this.bola.y + this.bola.altura))
+            {
+                this.bola.dirx=-1//Se não quiser que a bolinha tenha colisao traseira do jogador, basta colocar this.dirx = 1
+                this.bola.diry=((this.y+(this.altura/2))-(this.bola.y+(this.bola.altura/2)))/20
+
+        }
+    }
+
+    desenhar(){
+        this.gerenciar()
+        this.ctx.fillStyle="#000"
+        this.ctx.fillRect(this.x,this.y,this.largura,this.altura)
+    }
+    
+}

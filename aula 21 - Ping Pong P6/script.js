b3c644f657a68dab6b85c531af4607e3ead7fd71
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-04-07 10:02:30 -0300

    Criacao script.js

---
+++ aula 21 - Ping Pong P6/script.js
@@ -0,0 +1,57 @@
+let canvas = document.getElementById("desenho1")
+let ctx=canvas.getContext('2d')
+
+
+const teclado = {
+    cima:false,
+    baixo:false,
+    esquerda:false,
+    direita:false
+}
+
+const jogador = new Pad(ctx, teclado)
+const bola = new Bola(ctx, jogador)
+const cpu = new Cpu(ctx, bola)
+
+window.addEventListener('keydown', (event)=>{
+    if(event.keyCode == 37){ //Esquerda
+        teclado.esquerda = true
+    }else if (event.keyCode == 39){ //Direita
+        teclado.direita = true
+    }
+
+    if(event.keyCode == 38){ //Cima
+        teclado.cima = true
+    }else if (event.keyCode == 40){ //Baixo
+        teclado.baixo = true
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    if(event.keyCode == 37){ //Esquerda
+        teclado.esquerda = false
+    }else if (event.keyCode == 39){ //Direita
+        teclado.direita = false
+    }
+
+    if(event.keyCode == 38){ //Cima
+        teclado.cima = false
+    }else if (event.keyCode == 40){ //Baixo
+        teclado.baixo = false
+    }
+})
+
+const game=()=>{
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    jogador.desenhar()
+    bola.desenhar()
+    cpu.desenhar()
+    requestAnimationFrame(game)
+    
+}
+
+requestAnimationFrame(game)
+
+
+
+ 

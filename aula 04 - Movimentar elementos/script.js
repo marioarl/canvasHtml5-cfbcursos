1ae87a28d6477d1b5a5990ef278979deae2ced78
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-21 09:57:12 -0300

    Update script.js

--- aula 04 - Movimentar elementos/script.js
+++ aula 04 - Movimentar elementos/script.js
@@ -7,7 +7,7 @@ function desenha() {
     var ctx = canvas.getContext('2d')
     ctx.clearRect(0,0,500,500)    
 
-    ctx.fillStyle='#f00'
+    ctx.fillStyle='#f00' //Define a cor
     ctx.fillRect(px,0,50,50)
 
     px++

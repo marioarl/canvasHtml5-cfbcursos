3895d35c4f950d850716fb848326c1cb9caaa26c
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-21 09:55:58 -0300

    Update script.js

--- aula 02 - rect clearRect fillRect/script.js
+++ aula 02 - rect clearRect fillRect/script.js
@@ -5,7 +5,7 @@ var ctx=canvas.getContext('2d')
 ctx.fillStyle="#00f" 
 ctx.rect(0,0,100,100)
 
-ctx.fillStyle="#f00" //CUIDADO ! Se colocarmos outro comando de desenhar um retangulo o fill irá considerar o ultimo fillstyle e preencher com o elemento vermelho 
+ctx.fillStyle="#f00" //CUIDADO !Se colocarmos outro comando de desenhar um retangulo o fill irá considerar o ultimo fillstyle e preencher com o elemento vermelho 
 ctx.rect(50,50,100,100)
 //Para preencher temos que utilizar o fill
 ctx.fill()

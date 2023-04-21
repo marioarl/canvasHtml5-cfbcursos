0d316c710b42ebe42c7d46d327e1b596eaaa5f7e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-21 09:53:24 -0300

    Update script.js

--- aula 01 - Introducao/script.js
+++ aula 01 - Introducao/script.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("desenho1") //apontar o canvas
 var ctx=canvas.getContext('2d')//Informar o contexto do canvas(mode de trabalho)
 
 ctx.fillStyle="rgb(0,0,255)" //Definir um preenchimento para o elemento grafico(contexto)
-ctx.fillRect(0,0,100,100)//Desenhando o elemento quadrado - nos parenteses, deverá ser informada a posicao, largura e altura do desenho dentro do canvas
+ctx.fillRect(0,0,100,100)//Desenhando o elemento quadrado - nos parenteses, deverá ser informada a posicao, largura e altura do desenho dentro do canvas.
 
 ctx.fillStyle="rgba(255,0,0, 0.5)"
 ctx.fillRect(50,50,100,100)
